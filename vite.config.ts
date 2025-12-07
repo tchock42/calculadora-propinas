1811814cb4fcd98c591660626314da76f7ff4095
Jacob Gomez Carrillo <husky-coldjacob@hotmail.com>
2025-12-07 14:19:06 -0600

    Added vitest.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,4 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  test: {
-    globals: true,        // habilita describe, it, expect sin necesidad de importarlos
-    environment: 'jsdom', // simula navegador, necesario para probar hooks/componentes React
-    include: ['src/**/*.test.{ts,tsx}'], // opcional: define dónde buscar tests
-    exclude: ['node_modules', 'dist']    // opcional: evita carpetas innecesarias
-  }
 })
